f93a385f717d42feae8a46bf3fe092e78cf4e709
MDTVisit <112145096+MDTVisit@users.noreply.github.com>
2022-12-09 16:20:00 +0700

    aadadwdasdawd

--- Program.cs
+++ Program.cs
@@ -4,29 +4,34 @@ static void Main (string[] args)
 {
        Stack<char> a = new Stack<char>();
        Stack2 <char> b = new Stack2<char>();
-
+int i=0;
+int j =0;
 
        
        while (true) {
        char c = char.Parse(Console.ReadLine());
-     if (c == '(') {a.Push('(');}
-  
-     if (c == ')') {b.Push(')');} 
+ switch(c) {
 
-    else if (c != ')' && c != '(');
+ case '(':
+        i++;
+        break;
 
- }
-  if (a.GetLength == b.GetLength) {b.Push('A');}
+ case ')':
+         j++;
+        break;
+        
+default:
+if (i == j)
+Console.WriteLine("Valid");
 
+else if (i != j)
 
-  else
-  {
-      {a.Push('B');}
-
-  }
-
-
-}             
+Console.WriteLine("Invalid");
+break;
 }
 
 
+
+}
+}
+}
